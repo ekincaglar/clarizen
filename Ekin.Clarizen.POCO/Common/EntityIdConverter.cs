a1d773ea16e06990878efa3013dcdbf93ff67e45
Ekin Caglar <ekin@caglar.com>
2018-03-16 03:10:31 +0000

    EntityIdConverter improvements

---
+++ Ekin.Clarizen.POCO/Common/EntityIdConverter.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace Ekin.Clarizen.POCO.Common
+{
+    public class EntityIdConverter : JsonConverter
+    {
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            if (value is EntityId)
+            {
+                writer.WriteValue(((EntityId)value).id);
+            }
+            else
+            {
+                writer.WriteValue(value.ToString());
+            }
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            try
+            {
+                if (objectType == typeof(EntityId))
+                {
+                    if (reader.Value != null && !string.IsNullOrEmpty(reader.Value.ToString()))
+                    {
+                        return new EntityId(reader.Value?.ToString());
+                    }
+                    else
+                    {
+                        var result = serializer.Deserialize<EntityId>(reader);
+                        return result;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                if (reader.Value != null && !string.IsNullOrEmpty(reader.Value.ToString()))
+                {
+                    return new EntityId(reader.Value?.ToString());
+                }
+                return null;
+            }
+
+            return null;
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return true;
+        }
+    }
+}
