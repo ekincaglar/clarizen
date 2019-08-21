1950af600d61e48323dce2f68f1a60fc72f7aa7a
Ekin Caglar <ekincaglar@outlook.com>
2019-08-21 22:27:06 +0300

    missingTimesheets results are added

---
+++ Ekin.Clarizen.POCO/Common/dayOfMissingTimesheets.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ekin.Clarizen
+{
+    public class dayOfMissingTimesheets
+    {
+        public DateTime? date { get; set; }
+        public double? workingHours { get; set; }
+        public double? reportedHours { get; set; }
+        public double? missingHours { get; set; }
+
+        public dayOfMissingTimesheets() { }
+    }
+}
---
+++ Ekin.Clarizen.POCO/Responses/Data/getMissingTimesheets.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ekin.Clarizen.POCO.Responses.Data
+{
+    public class getMissingTimesheets
+    {
+        public List<dayOfMissingTimesheets> missingTimesheets { get; set; }
+
+        public getMissingTimesheets() { }
+    }
+}
