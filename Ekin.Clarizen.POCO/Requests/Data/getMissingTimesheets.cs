d13deec5287ac4136a7d497f75a53cbbfd6bfcb8
Ekin Caglar <ekincaglar@outlook.com>
2019-08-21 22:48:59 +0300

    missingTimesheet result added and request namespace corrected

---
+++ Ekin.Clarizen.POCO/Requests/Data/getMissingTimesheets.cs
@@ -0,0 +1,49 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Serialization;
+
+namespace Ekin.Clarizen.Data.Request
+{
+    public class getMissingTimesheets
+    {
+        /// <summary>
+        /// EntityId of the User. If querying a multi-instance environment use /Organization/orgId/User/userId
+        /// </summary>
+        public string user { get; set; }
+
+        /// <summary>
+        /// The start of the date range (inclusive)
+        /// </summary>
+        public DateTime startDate { get; set; }
+
+        /// <summary>
+        /// The end of the date range (exclusive)
+        /// </summary>
+        public DateTime endDate { get; set; }
+
+        /// <summary>
+        /// (Optional) Tolerance in minutes. Default is 5 minutes.
+        /// </summary>
+        public int? tolerance { get; set; } = null;
+
+        public getMissingTimesheets() { }
+
+        public getMissingTimesheets(string user, DateTime startDate, DateTime endDate)
+        {
+            this.user = user;
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
+
+        public getMissingTimesheets(string user, DateTime startDate, DateTime endDate, int tolerance)
+        {
+            this.user = user;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.tolerance = tolerance;
+        }
+    }
+}
--- Ekin.Clarizen.POCO/Responses/Data/getMissingTimesheets.cs
+++ Ekin.Clarizen.POCO/Responses/Data/getMissingTimesheets.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Ekin.Clarizen.POCO.Responses.Data
+namespace Ekin.Clarizen.Data.Result
 {
     public class getMissingTimesheets
     {
