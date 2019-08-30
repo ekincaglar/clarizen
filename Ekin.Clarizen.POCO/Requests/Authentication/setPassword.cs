96e70baea942836257f3f3e069c1067856746743
Ekin Caglar <ekincaglar@outlook.com>
2019-08-30 10:39:27 +0300

    setPassword request added

---
+++ Ekin.Clarizen.POCO/Requests/Authentication/setPassword.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ekin.Clarizen.Authentication.Request
+{
+    public class setPassword
+    {
+        /// <summary>
+        /// Fully qualified Id of the user, e.g. /Organization/SomeOrgId/User/SomeUserId
+        /// </summary>
+        public string userId;
+
+        /// <summary>
+        /// The password
+        /// </summary>
+        public string newPassword;
+
+        public setPassword(string userId, string newPassword)
+        {
+            this.userId = userId;
+            this.newPassword = newPassword;
+        }
+    }
+}
