ce558cd6f115d787ae141b6667b5ba95a608cd36
Bruce Bennett <Bruce.Bennett@clarizen.com>
2019-11-20 11:45:41 +0000

    Logout of the API if connected.

--- Clarizen.Tests/Context/Context.cs
+++ Clarizen.Tests/Context/Context.cs
@@ -1,12 +1,15 @@
-﻿using Clarizen.Tests.Models;
+﻿using System;
 using Ekin.Clarizen;
-using Ekin.Clarizen.Data;
 
 namespace Clarizen.Tests.Context
 {
-    public class BaseContext
+    public class BaseContext:IDisposable
     {
         public API Api { get; set; }
         public string ProjectId { get; internal set; }
+        public void Dispose()
+        {
+            Api?.Logout();
+        }
     }
 }
