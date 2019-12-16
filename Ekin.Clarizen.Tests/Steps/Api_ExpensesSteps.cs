f1c4386ba788ab3ae42572b4b0d32073b561b17c
Bruce Bennett <Bruce.Bennett@clarizen.com>
2019-12-16 16:11:21 +0000

    Add Empty Api_Expenses feature and steps

---
+++ Ekin.Clarizen.Tests/Steps/Api_ExpensesSteps.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ekin.Clarizen.Tests.Context;
+using TechTalk.SpecFlow;
+using Xunit;
+using System.Xml.Linq;
+namespace Ekin.Clarizen.Tests.Steps
+{
+    [Binding]
+    public  class Api_ExpensesSteps : BaseApiSteps
+    {
+        public Api_ExpensesSteps(BaseContext context) : base(context)
+        {
+        }
+
+
+    }
+}
