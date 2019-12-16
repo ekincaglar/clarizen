1c9186ace2201f56ec1e5e16e7aa9dd37c534bcb
Bruce Bennett <Bruce.Bennett@clarizen.com>
2019-12-16 13:43:10 +0000

    Add empty Api_MetaData feature and steps

---
+++ Ekin.Clarizen.Tests/Features/Api_MetaData.feature
@@ -0,0 +1,8 @@
+﻿Feature: MetaDataTests
+	Test the metadata Calls
+
+Background: 
+	Given I Login using credentials in appsettings
+
+Scenario:  Describe the weekDays entity
+	Given I call the weekdays entity
---
+++ Ekin.Clarizen.Tests/Steps/Api_MetaDataSteps.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Ekin.Clarizen.Tests.Context;
+using TechTalk.SpecFlow;
+
+namespace Ekin.Clarizen.Tests.Steps
+{
+    [Binding]
+    public  class Api_MetaDataSteps : BaseApiSteps
+    {
+        public Api_MetaDataSteps(BaseContext context) : base(context)
+        {
+        }
+
+        [Given(@"I call the weekdays entity")]
+        public void GivenICallTheWeekdaysEntity()
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
