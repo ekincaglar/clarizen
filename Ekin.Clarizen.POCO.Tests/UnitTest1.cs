63be5f88ad3c6e52338d7251f938ed2a2e05cc93
Bruce Bennett <Bruce.Bennett@clarizen.com>
2019-12-23 08:58:28 +0000

    Add empty xunit project

---
+++ Ekin.Clarizen.POCO.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Ekin.Clarizen.POCO.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
