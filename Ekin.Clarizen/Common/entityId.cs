093bd9e5da62c8c9b7dd33d25b3307e166819f55
Ekin Caglar <ekin@caglar.com>
2020-07-09 20:25:20 +0100

    Id_value calculation in EntityId changed. It now splits the string after the second slash, instead of the last slash. EntityId.GetMultiSelectPickListIds() function added to use the new Id_value.

--- Ekin.Clarizen/Common/entityId.cs
+++ Ekin.Clarizen/Common/entityId.cs
@@ -1,20 +1,42 @@
 ﻿using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Ekin.Clarizen
 {
     public class EntityId
     {
-        public string Id { get; set; }
-
-        [JsonIgnore]
-        public string Id_value
+        private string _id { get; set; }
+        public string Id
         {
             get
             {
-                return !string.IsNullOrWhiteSpace(Id) && Id.Contains("/") ? Id.Substring(Id.LastIndexOf("/") + 1) : Id;
+                return _id;
+            }
+            set
+            {
+                _id = value;
+
+                // Find the text after the second slash in the Id and set it to the Id_value property
+                if (!string.IsNullOrWhiteSpace(_id))
+                {
+                    // Apologies for the lack of syntactic sugar but the below algorithm is, sadly, faster than a LINQ or a Regex solution
+                    int firstSlash = _id.IndexOf('/');
+                    if (firstSlash >= 0)
+                    {
+                        int secondSlash = _id.IndexOf('/', firstSlash + 1);
+                        if (secondSlash >= 0)
+                        {
+                            Id_value = _id.Length > secondSlash + 1 ? _id.Substring(secondSlash + 1) : string.Empty;
+                        }
+                    }
+                }
             }
         }
+
+        [JsonIgnore]
+        public string Id_value { get; private set; }
 
         public EntityId() { }
 
@@ -45,8 +67,10 @@ namespace Ekin.Clarizen
             return Id.GetHashCode();
         }
 
+        public List<string> GetMultiSelectPickListIds()
+        {
+            if (string.IsNullOrEmpty(Id_value)) return new List<string>();
+            return Id_value.Split(';').Select(str => str.Trim()).ToList();
+        }
     }
-
-
-
 }
