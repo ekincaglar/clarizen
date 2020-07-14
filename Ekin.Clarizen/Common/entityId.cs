73d098bf59cdd5d09882e191ad4b6fc673340f40
Ekin Caglar <ekin@caglar.com>
2020-07-14 11:58:08 +0100

    EntityId updated to force serialisation of id property in small letters and handling of FQIDs for Id_value parameter

--- Ekin.Clarizen/Common/entityId.cs
+++ Ekin.Clarizen/Common/entityId.cs
@@ -2,12 +2,17 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Xml.Serialization;
 
 namespace Ekin.Clarizen
 {
     public class EntityId
     {
         private string _id { get; set; }
+        private string _id_value { get; set; }
+
+        [XmlElement("id")]
+        [JsonProperty("id")]
         public string Id
         {
             get
@@ -17,26 +22,34 @@ namespace Ekin.Clarizen
             set
             {
                 _id = value;
-
-                // Find the text after the second slash in the Id and set it to the Id_value property
-                if (!string.IsNullOrWhiteSpace(_id))
-                {
-                    // Apologies for the lack of syntactic sugar but the below algorithm is, sadly, faster than a LINQ or a Regex solution
-                    int firstSlash = _id.IndexOf('/');
-                    if (firstSlash >= 0)
-                    {
-                        int secondSlash = _id.IndexOf('/', firstSlash + 1);
-                        if (secondSlash >= 0)
-                        {
-                            Id_value = _id.Length > secondSlash + 1 ? _id.Substring(secondSlash + 1) : string.Empty;
-                        }
-                    }
-                }
+                _id_value = null; // This will get calculated the first time Id_value property is accessed
             }
         }
 
         [JsonIgnore]
-        public string Id_value { get; private set; }
+        public string Id_value
+        {
+            get 
+            {
+                if (_id_value != null) return _id_value;
+
+                // Find the text after the second slash in the Id and set it to the Id_value property
+                if (!string.IsNullOrWhiteSpace(_id))
+                {
+                    string idToOperateOn = _id;
+
+                    // For Fully Qualified Ids (FQIDs) in a multi-org environment, ignore the Organization part of the Id
+                    if (idToOperateOn.StartsWith("/Organization/"))
+                    {
+                        idToOperateOn = GetValueAfterSecondSlash(_id_value);
+                    }
+
+                    _id_value = GetValueAfterSecondSlash(idToOperateOn);
+                }
+
+                return _id_value;
+            }
+        }
 
         public EntityId() { }
 
@@ -71,6 +84,24 @@ namespace Ekin.Clarizen
         {
             if (string.IsNullOrEmpty(Id_value)) return new List<string>();
             return Id_value.Split(';').Select(str => str.Trim()).ToList();
+        }
+
+        private string GetValueAfterSecondSlash(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value)) return string.Empty;
+
+            // Apologies for the lack of syntactic sugar but the below algorithm is, sadly, faster than a LINQ or a Regex solution
+            int firstSlash = value.IndexOf('/');
+            if (firstSlash >= 0)
+            {
+                int secondSlash = _id.IndexOf('/', firstSlash + 1);
+                if (secondSlash >= 0)
+                {
+                    return _id.Length > secondSlash + 1 ? _id.Substring(secondSlash + 1) : string.Empty;
+                }
+            }
+
+            return string.Empty;
         }
     }
 }
