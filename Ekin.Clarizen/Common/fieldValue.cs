2eac5964258b52eea6ceee9172c7685584682989
Rahul Singh <rahsingh28@publicisgroupe.net>
2022-01-29 01:10:34 +0530

    Updated type of Field value as Object

--- Ekin.Clarizen/Common/fieldValue.cs
+++ Ekin.Clarizen/Common/fieldValue.cs
@@ -3,11 +3,11 @@
     public class FieldValue
     {
         public string FieldName { get; set; }
-        public string Value { get; set; }
+        public object Value { get; set; }
 
         public FieldValue() { }
 
-        public FieldValue(string fieldName, string value)
+        public FieldValue(string fieldName, object value)
         {
             FieldName = fieldName;
             Value = value;
